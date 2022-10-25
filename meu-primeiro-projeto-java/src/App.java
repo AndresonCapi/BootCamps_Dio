d6deecf6b59b64720c48d2444dad5a31e2ac3706
AndresonCapi <andresoncapi@gmail.com>
2022-10-25 15:56:42 -0300

    segunda alteração de frase

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Boa vindo turma DIO!");
+        System.out.println("Boas vindas turma DIO!");
     }
 }
